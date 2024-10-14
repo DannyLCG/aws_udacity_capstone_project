1be318f91fcbb61d0d7a0c445d89d3e6ae934739
Danny <olopez@lcg.unam.mx>
2024-10-14 20:43:01 +0000

    Solved predicting issue. NEXT: add validation on benchmark.

--- submission_script.py
+++ submission_script.py
@@ -6,7 +6,7 @@ from sagemaker.debugger import ProfilerRule, ProfilerConfig, FrameworkProfile
 
 role = get_execution_role()
 # Set hyperparameters
-hyperparameters = {"epochs": "30",
+hyperparameters = {"epochs": "10",
                 "batch_size": "128",
                 "learning_rate": "0.01"}
 
@@ -24,10 +24,13 @@ rules = [
 ]
 
 # Set profiler and debugger configs
+#debugger_config = DebuggerHookConfig(
+#    collection_configs=[
+#        CollectionConfig(name="losses", parameters={"save_interval": "5"})
+#    ])
 debugger_config = DebuggerHookConfig(
-    collection_configs=[
-        CollectionConfig(name="losses", parameters={"save_interval": "5"})
-    ])
+    hook_parameters={"train.save_interval": "100", "eval.save_interval": "10"}
+)
 
 profiler_config = ProfilerConfig(
     system_monitor_interval_millis=1000,
@@ -39,21 +42,21 @@ estimator = PyTorch(
     entry_point="train_benchmark.py",
     base_job_name="benchmark-job",
     role=role,
-    instance_type="ml.m5.xlarge", #ml.g4dn.xlarge
+    instance_type="ml.g4dn.xlarge", #ml.g4dn.xlarge, ml.m5.xlarge
     instance_count=1,
     source_dir="./scripts",
     dependencies=["./scripts/requirements.txt"],
     hyperparameters=hyperparameters,
-    #debugger_hook_config=debugger_config, #Uncomment these lines to perform profiling and debug
-    #profiler_config=profiler_config, 
-    #rules=rules,
+    debugger_hook_config=debugger_config, #Uncomment these lines to perform profiling and debug
+    profiler_config=profiler_config, 
+    rules=rules,
     framework_version="2.2", #Pytorch version List of supported versions: https://github.com/aws/deep-learning-containers/blob/master/available_images.md.
     py_version="py310"
 )
 
 # Set the input channels usig the URIs from the uplpoad
-inputs = {'training': 's3://capstone42/data/train/ecoli_train_im.csv',
-          'test': 's3://capstone42/data/test/ecoli_test_im.csv'}
+inputs = {'training': 's3://capstone520/data/train/ecoli_train_im.csv',
+          'test': 's3://capstone520/data/test/ecoli_test_im.csv'}
 
 # Launch the training job
 estimator.fit(inputs, wait=True)
